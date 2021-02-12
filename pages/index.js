d66852e6f8bd17f0f60e56393e83ee3dc5797cf7
yevhenii-pyl <flyhighthendie@gmail.com>
2021-02-12 13:48:27 +0200

    converted a few files to typescript versions

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Layout, { siteTitle } from '../components/layout.tsx'
 import utilStyles from '../styles/utils.module.css'
 import {getSortedPostsData} from '../lib/posts'
 import Link from 'next/link'
-import Date from '../components/date'
+import Date from '../components/date.tsx'
 
 export async function getStaticProps(){
   const allPostsData = getSortedPostsData()
@@ -21,11 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Some Self Introduction</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingLg}>Blog</h2>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
-import Layout from '../../components/layout'
+import Layout from '../../components/layout.tsx'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
-import Date from '../../components/date'
+import Date from '../../components/date.tsx'
 import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
